8e363385384ac4baa14eb70ea6485c481c1b0eae
McDonald Namba <nambamcdonald@gmail.com>
2023-05-16 17:06:27 +0200

    to linux desktop

--- lib/widgets/myTabBarViews.dart
+++ lib/widgets/myTabBarViews.dart
@@ -13,10 +13,32 @@ class myTabBarViews extends StatelessWidget {
     return SizedBox(
       height: double.maxFinite,
       width: double.maxFinite,
-      child: TabBarView(controller: tabController, children: const [
-        Text('Rooms'),
-        Text('Restaurant'),
-        Text('Conference')
+      child: TabBarView(controller: tabController, children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: GridView.count(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 7,
+            childAspectRatio: 0.75,
+            children: List.generate(10, (index) {
+              return GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(context, 'details');
+                  },
+                  child: Container(
+                    height: 100,
+                    width: 50,
+                    decoration: const BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ));
+            }),
+          ),
+        ),
+        const Text('Restaurant'),
+        const Text('Conference')
       ]),
     );
   }
